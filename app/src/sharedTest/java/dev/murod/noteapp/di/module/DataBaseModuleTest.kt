d814ad2edeb45d464b5ccda169cf460f8576e074
minmax9493 <min_max1993@mail.ru>
2022-01-23 23:30:09 +0900

    prepare modules for wiritng tests

--- app/src/sharedTest/java/dev/murod/noteapp/di/module/DataBaseModuleTest.kt
+++ app/src/sharedTest/java/dev/murod/noteapp/di/module/DataBaseModuleTest.kt
@@ -1,4 +1,29 @@
 package dev.murod.noteapp.di.module
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import dev.murod.noteapp.data.source.local.dao.NoteDao
+import dev.murod.noteapp.data.source.local.db.NoteDatabase
+
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DataBaseModule::class]
+)
+@Module
 class DataBaseModuleTest {
+
+    @Provides
+    fun provideNoteDatabase(@ApplicationContext context: Context): NoteDatabase = Room
+        .inMemoryDatabaseBuilder(context, NoteDatabase::class.java)
+        .allowMainThreadQueries()// in Unit test try to work on the same thread, since multithreading manipulates each other
+        .build()
+
+    @Provides
+    fun provideNoteDao(database: NoteDatabase): NoteDao = database.noteDao()
+
 }
--- app/src/test/java/dev/murod/noteapp/data/source/local/dao/NoteDaoTest.kt
+++ app/src/test/java/dev/murod/noteapp/data/source/local/dao/NoteDaoTest.kt
@@ -1,4 +1,45 @@
 package dev.murod.noteapp.data.source.local.dao
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import dev.murod.noteapp.data.source.local.db.NoteDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(RobolectricTestRunner::class)
+@ExperimentalCoroutinesApi
+@Config(application = HiltTestApplication::class, sdk = [29])
 class NoteDaoTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var noteDatabase: NoteDatabase
+
+    lateinit var noteDao: NoteDao
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        noteDao = noteDatabase.noteDao()
+    }
+
+    @After
+    fun tearDown() {
+        noteDatabase.close()
+    }
 }
